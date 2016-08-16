7e46ca3a1fb801a7df3af8a64af73e3856dbefcc
andres garcia, Rebecca Allen and Sam Peppard <andres@>
2016-08-16 10:05:03 -0400

    finish count project with for looping

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
-  $("#form").submit(function(event) {
+  $("form#form").submit(function(event) {
     event.preventDefault();
-    
+
+    var firstNumber = parseInt($("input#first").val());
+    var secondNumber = parseInt($("input#second").val());
+    var total = 0;
+    var output = [];
+
+
+    for (var index = secondNumber; index <= firstNumber; index += secondNumber) {
+    console.log(index);
+
+      output.push(index);
+      total += index;
+    }
+
+    alert(output);
+    alert(total);
+
+
   });
 });
