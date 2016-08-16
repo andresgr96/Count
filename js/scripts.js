daa38f9364e4930956523c40453b6508cb344b58
andres garcia, Rebecca Allen and Sam Peppard <sam@>
2016-08-16 10:38:34 -0400

    clean up edge cases and succesfully displayed result

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,27 @@ $(document).ready(function() {
     var total = 0;
     var output = [];
 
-
-    for (var index = secondNumber; index <= firstNumber; index += secondNumber) {
+    if ( secondNumber > firstNumber) {
+      alert("The second number cannot be greater than the first!")
+    }
+    else if ( !firstNumber || !secondNumber){
+      alert("You have to input numbers!")
+    }
+    else if(firstNumber < 0 || secondNumber < 0){
+      alert("Your numbers have to be positive!")
+    }
+    else {
+      for (var index = secondNumber; index <= firstNumber; index += secondNumber) {
     console.log(index);
 
       output.push(index);
       total += index;
     }
 
+    }
+
     alert(output);
-    alert(total);
+    console.log(total);
 
 
   });
